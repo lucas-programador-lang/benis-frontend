73237a5e03eed5fde2ee89c2771914140a3dc92c
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-03 20:37:34 -0400

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const API_URL = "https://benis-backend.onrender.com"
 
+const phone = "5569993668336"
+
 const products = [
+    {name:"Misto Quente",price:7},
+    {name:"X-Bauru",price:8},
     {name:"X-Burger",price:13},
-    {name:"X-Bacon",price:19},
+    {name:"X-Salada",price:14},
     {name:"X-Salada Especial",price:17},
+    {name:"X-Calabresa",price:18},
+    {name:"X-Bacon",price:19},
+    {name:"X-Franbacon",price:20},
+    {name:"X-Frango",price:18},
+    {name:"X-Turbinado",price:20},
+    {name:"X-Bagunça",price:20},
     {name:"X-Tudo",price:23},
+    {name:"X-Havaiano",price:19},
+    {name:"X-Benis",price:26},
     {name:"Batata Frita",price:15},
-    {name:"X-Frango",price:20}
+    {name:"Batata + Cheddar + Bacon",price:25},
+    {name:"Coca Cola 2L",price:15},
+    {name:"Coca Cola 1L",price:10},
+    {name:"Tuchaua 2L",price:9},
+    {name:"Dydyo 2L",price:9}
 ]
 
 let cart = []
@@ -18,9 +34,7 @@ products.forEach(product=>{
         <div class="card">
             <h3>${product.name}</h3>
             <p>R$ ${product.price.toFixed(2)}</p>
-            <button onclick="addToCart('${product.name}',${product.price})">
-                Adicionar
-            </button>
+            <button onclick="addToCart('${product.name}',${product.price})">Adicionar</button>
         </div>
     `
 })
@@ -46,44 +60,24 @@ function updateCart(){
 
 function toggleCart(){
     const modal=document.getElementById("cart-modal")
-    modal.style.display = modal.style.display==="flex" ? "none" : "flex"
+    modal.classList.toggle("active")
 }
 
-async function checkout(){
+function checkout(){
     if(cart.length===0){
         alert("Carrinho vazio")
         return
     }
 
-    let total = 0
-    cart.forEach(item=> total += item.price)
+    let message="🍔 Pedido Benis Burguer:%0A"
+    let total=0
 
-    const newOrder = {
-        items: cart,
-        total: total
-    }
+    cart.forEach(item=>{
+        message+=`${item.name} - R$ ${item.price.toFixed(2)}%0A`
+        total+=item.price
+    })
 
-    try{
-        const response = await fetch(`${API_URL}/orders`,{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify(newOrder)
-        })
+    message+=`%0ATotal: R$ ${total.toFixed(2)}`
 
-        if(!response.ok){
-            throw new Error("Erro ao enviar pedido")
-        }
-
-        alert("Pedido enviado com sucesso!")
-
-        cart = []
-        updateCart()
-        toggleCart()
-
-    }catch(error){
-        console.error(error)
-        alert("Erro ao enviar pedido")
-    }
+    window.open(`https://wa.me/${phone}?text=${message}`)
 }
