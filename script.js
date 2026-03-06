fc4b3ecfdd6767d6e4cfc408895574f022b1c54d
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-06 17:37:12 -0400

    script.js

--- script.js
+++ script.js
@@ -1,74 +1,40 @@
-
-const produtos = [
-
-{name:"Misto Quente",preco:7},
-
-{name:"X-Bauru",preco:8},
+const produtos=[
 
 {name:"X-Burguer",preco:13},
 
 {name:"X-Salada",preco:14},
 
-{name:"X-Salada Especial",preco:17},
-
 {name:"X-Calabresa",preco:18},
 
 {name:"X-Bacon",preco:19},
 
-{name:"X-Franbacon",preco:20},
-
-{name:"X-Franbesa",preco:20},
-
 {name:"X-Frango",preco:18},
-
-{name:"X-Turbinado",preco:20},
-
-{name:"X-Bagunça",preco:20},
 
 {name:"X-Tudo",preco:23},
 
-{name:"X-Havaiano",preco:19},
-
-{name:"X-Benis",preco:26},
-
 {name:"Batata Frita",preco:15},
 
-{name:"Batata + Cheddar + Bacon",preco:25},
-
-{name:"Coca Cola 2L",preco:15},
-
-{name:"Coca Cola 1L",preco:10},
-
-{name:"Tuchaua 2L",preco:9},
-
-{name:"Dydyo 2L",preco:9},
-
-{name:"Coca Cola Lata",preco:7}
+{name:"Coca Cola 2L",preco:15}
 
 ]
 
-
 let carrinho=[]
 
-
-const lista=document.getElementById("produtos")
-
+const menu=document.getElementById("menu")
 
 produtos.forEach((p,i)=>{
 
-const div=document.createElement("div")
+let div=document.createElement("div")
 
-div.className="produto"
+div.className="item"
 
-div.innerHTML=`
+div.innerHTML=
 
-<span>${p.name} - R$${p.preco}</span>
+`${p.name} - R$${p.preco}
 
-<button onclick="add(${i})">Adicionar</button>
+<button onclick="add(${i})">Adicionar</button>`
 
-`
-
-lista.appendChild(div)
+menu.appendChild(div)
 
 })
 
@@ -77,14 +43,14 @@ function add(i){
 
 carrinho.push(produtos[i])
 
-renderCart()
+render()
 
 }
 
 
-function renderCart(){
+function render(){
 
-const cart=document.getElementById("cartItems")
+const cart=document.getElementById("cart")
 
 cart.innerHTML=""
 
@@ -92,9 +58,9 @@ let total=0
 
 carrinho.forEach(p=>{
 
-const li=document.createElement("li")
+let li=document.createElement("li")
 
-li.innerText=p.name+" - R$"+p.preco
+li.innerText=p.name+" R$"+p.preco
 
 cart.appendChild(li)
 
@@ -111,28 +77,28 @@ function aberto(){
 
 const agora=new Date()
 
-const hora=agora.getHours()
+const h=agora.getHours()
 
-const dia=agora.getDay()
+const d=agora.getDay()
 
-if(dia==1) return false
+if(d==1)return false
 
-return hora>=19 && hora<=23
+return h>=19 && h<=23
 
 }
 
 
-document.getElementById("checkout").onclick=()=>{
+function finalizar(){
 
 if(!aberto()){
 
-alert("Restaurante fechado!")
+alert("Estamos fechados")
 
 return
 
 }
 
-let mensagem="Pedido Benis Burguer:%0A"
+let mensagem="Pedido Benis Burguer%0A"
 
 carrinho.forEach(p=>{
 
@@ -147,18 +113,3 @@ window.open(
 )
 
 }
-
-
-function status(){
-
-const abertoAgora=aberto()
-
-document.getElementById("status").innerText=
-
-abertoAgora ? "🟢 Aberto agora" : "🔴 Fechado"
-
-}
-
-setInterval(status,1000)
-
-status()
