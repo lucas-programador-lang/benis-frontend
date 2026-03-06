c37620fabbd344a357d54ddfb95317cb3c442351
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-06 19:11:33 -0400

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,35 @@
-const CACHE_NAME = "benis-burguer-v1";
+const CACHE = "benis-v1"
 
-const urlsToCache = [
-
+const urls = [
 "/",
 "/frontend/index.html",
 "/frontend/style.css",
 "/frontend/app.js",
-"/manifest.json"
+"/frontend/mapa.js",
+"/frontend/logo.png"
+]
 
-];
-
-self.addEventListener("install", event => {
+self.addEventListener("install",event=>{
 
 event.waitUntil(
 
-caches.open(CACHE_NAME)
+caches.open(CACHE).then(cache=>{
+return cache.addAll(urls)
+})
 
-.then(cache => {
-
-return cache.addAll(urlsToCache);
+)
 
 })
 
-);
-
-});
-
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch",event=>{
 
 event.respondWith(
 
 caches.match(event.request)
-
-.then(response => {
-
-if(response){
-
-return response;
-
-}
-
-return fetch(event.request);
-
+.then(response=>{
+return response || fetch(event.request)
 })
 
-);
+)
 
-});
+})
