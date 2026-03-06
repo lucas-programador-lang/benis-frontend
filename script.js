a1342044296feee7fa01c952b95e6b16292a764c
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-03-06 17:34:34 -0400

    script.js

--- script.js
+++ script.js
@@ -2,39 +2,63 @@
 const produtos = [
 
 {name:"Misto Quente",preco:7},
+
 {name:"X-Bauru",preco:8},
+
 {name:"X-Burguer",preco:13},
+
 {name:"X-Salada",preco:14},
+
 {name:"X-Salada Especial",preco:17},
+
 {name:"X-Calabresa",preco:18},
+
 {name:"X-Bacon",preco:19},
+
 {name:"X-Franbacon",preco:20},
+
 {name:"X-Franbesa",preco:20},
+
 {name:"X-Frango",preco:18},
+
 {name:"X-Turbinado",preco:20},
+
 {name:"X-Bagunça",preco:20},
+
 {name:"X-Tudo",preco:23},
-{name:"X-Havaino",preco:19},
+
+{name:"X-Havaiano",preco:19},
+
 {name:"X-Benis",preco:26},
+
 {name:"Batata Frita",preco:15},
+
 {name:"Batata + Cheddar + Bacon",preco:25},
+
 {name:"Coca Cola 2L",preco:15},
+
 {name:"Coca Cola 1L",preco:10},
+
 {name:"Tuchaua 2L",preco:9},
+
 {name:"Dydyo 2L",preco:9},
-{name:"Coca Lata",preco:7}
 
-];
+{name:"Coca Cola Lata",preco:7}
 
-let carrinho=[];
+]
 
-const produtosDiv=document.getElementById("produtos");
+
+let carrinho=[]
+
+
+const lista=document.getElementById("produtos")
+
 
 produtos.forEach((p,i)=>{
 
-const div=document.createElement("div");
+const div=document.createElement("div")
 
-div.className="produto";
+div.className="produto"
 
 div.innerHTML=`
 
@@ -42,82 +66,99 @@ div.innerHTML=`
 
 <button onclick="add(${i})">Adicionar</button>
 
-`;
+`
 
-produtosDiv.appendChild(div);
+lista.appendChild(div)
 
-});
+})
+
 
 function add(i){
 
-carrinho.push(produtos[i]);
+carrinho.push(produtos[i])
 
-renderCart();
+renderCart()
 
 }
+
 
 function renderCart(){
 
-const cart=document.getElementById("cartItems");
+const cart=document.getElementById("cartItems")
 
-cart.innerHTML="";
+cart.innerHTML=""
 
-let total=0;
+let total=0
 
 carrinho.forEach(p=>{
 
-const li=document.createElement("li");
+const li=document.createElement("li")
 
-li.innerText=p.name+" - R$"+p.preco;
+li.innerText=p.name+" - R$"+p.preco
 
-cart.appendChild(li);
+cart.appendChild(li)
 
-total+=p.preco;
+total+=p.preco
 
-});
+})
 
-document.getElementById("total").innerText=total;
+document.getElementById("total").innerText=total
 
 }
+
 
 function aberto(){
 
-const agora=new Date();
+const agora=new Date()
 
-const hora=agora.getHours();
+const hora=agora.getHours()
 
-const dia=agora.getDay();
+const dia=agora.getDay()
 
-if(dia==1) return false;
+if(dia==1) return false
 
-return hora>=19 && hora<=23;
+return hora>=19 && hora<=23
 
 }
+
 
 document.getElementById("checkout").onclick=()=>{
 
 if(!aberto()){
 
-alert("Restaurante fechado!");
+alert("Restaurante fechado!")
 
-return;
+return
+
+}
+
+let mensagem="Pedido Benis Burguer:%0A"
+
+carrinho.forEach(p=>{
+
+mensagem+=p.name+"%0A"
+
+})
+
+window.open(
+
+"https://wa.me/556993668336?text="+mensagem
+
+)
 
 }
 
-alert("Pedido enviado!");
-
-}
 
 function status(){
 
-const abertoAgora=aberto();
+const abertoAgora=aberto()
 
 document.getElementById("status").innerText=
 
-abertoAgora ? "🟢 Aberto agora" : "🔴 Fechado";
+abertoAgora ? "🟢 Aberto agora" : "🔴 Fechado"
 
 }
 
-setInterval(status,1000);
+setInterval(status,1000)
 
-status();
+status()
